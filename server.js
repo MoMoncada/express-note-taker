96b919498c0642d19e0b167a0a6c7d266fbb9f9a
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-02-08 16:43:52 +1100

    template for middleware functions

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ var uuuidv1 = require('uuidv1');
 //--- setting PORT 3001 and initializing app ---//
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//--- TODO: set Middleware Functions---//
+// // parse incoming string from the client
+// app.use(express.urlencoded({ extended: true }));
+// // serve the contents of the public directory as static files
+// app.use(express.json());
+// app.use(express.static("public"));

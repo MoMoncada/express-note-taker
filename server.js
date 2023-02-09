194a4d8c1631d0625fd2540260724a45a08da8ae
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-02-09 14:33:44 +1100

    added rough todos steps to complete project on time

--- server.js
+++ server.js
@@ -10,8 +10,22 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //--- TODO: set Middleware Functions---//
-// // parse incoming string from the client
-// app.use(express.urlencoded({ extended: true }));
-// // serve the contents of the public directory as static files
-// app.use(express.json());
-// app.use(express.static("public"));
+
+    //-- parsing incoming string/ data from the client
+app.use(express.urlencoded({ extended: true }));
+    //-- serve the contents of the public directory as static files
+app.use(express.json());
+app.use(express.static("public"));
+
+// TODO: define the routing
+
+    // TODO: GET for '/notes'
+
+    // TODO: GET for '/api/notes' from the db.json file and sends a JSON response
+
+    // TODO: POST route for `/api/notes` that receives a JSON object from the request body, adds an ID to it, writes the new data to the "db.json" file, and sends a success message as a JSON response
+
+    // TODO: catch-all GET route for any other endpoint that sends the `index.html` file located in the "public" folder
+
+    // TODO: BONUS ROUND: DELETE route for '/api/notes/:id' that deletes the note with the specified ID from the "db.json" file
+    

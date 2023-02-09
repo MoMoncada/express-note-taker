289f444aab967f7fa3693f1dbcf50cee04fb65a3
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-02-09 16:25:19 +1100

    added GET and POST routes for /notes/api, tested it adding a note with Insomnia and was added to the db.json file

--- server.js
+++ server.js
@@ -25,8 +25,22 @@ app.use(express.static("public"));
     });
 
     // TODO: GET for '/api/notes' from the db.json file and sends a JSON response
+    app.get('/api/notes',(req,res)=>{
+        const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8');
+        const parseNotes = JSON.parse(dataNotes);
+        res.json(parseNotes);
+    });
 
     // TODO: POST route for `/api/notes` that receives a JSON object from the request body, adds an ID to it, writes the new data to the "db.json" file, and sends a success message as a JSON response
+    app.post('/api/notes',(req,res) => {
+        const dataNotes = fs.readFileSync(path.join(__dirname,'./db/db.json'),'utf8');
+        const parseNotes = JSON.parse(dataNotes);
+        req.body.id = uuuidv1();
+        parseNotes.push(req.body);
+
+        fs.writeFileSync(path.join(__dirname,'./db/db.json'), JSON.stringify(parseNotes), 'utf8');
+        res.json('You have added a new note!');
+    });
 
     // TODO: catch-all GET route for any other endpoint that sends the `index.html` file located in the "public" folder
 
